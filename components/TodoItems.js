8d2010bb16374bfc72f546ad9b6b4ca180e99d4e
Shivanshu Chourasia <shivansh4011@gmail.com>
2019-12-24 12:25:03 +0530

    Add conditional rendering on todo items

--- components/TodoItems.js
+++ components/TodoItems.js
@@ -4,8 +4,22 @@ export default (props) => {
 	return (
 		<div className="form-check mb-2">
 			<div className="input-group-label">
-				<input type="checkbox" className="form-check-input" />
-				<p className="m-2">{props.item.text}</p>
+				<input
+					type="checkbox"
+					className="form-check-input"
+					onChange={() => props.handleChange(props.item.id)}
+					checked={props.item.completed}
+				/>
+				<p
+					style={{
+						textDecoration: props.item.completed ? 'line-through' : 'none',
+						color: props.item.completed ? '#cdcdcd' : 'black',
+						fontStyle: props.item.completed ? 'italic' : null
+					}}
+					className="m-2"
+				>
+					{props.item.text}
+				</p>
 			</div>
 			<hr />
 		</div>
--- src/App.js
+++ src/App.js
@@ -1,16 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import todosData from '../data/todosData';
 import TodoItems from '../components/TodoItems';
 
-const App = () => {
-	const todoItems = todosData.map((item) => <TodoItems key={item.id} item={item} />);
+class App extends Component {
+	constructor() {
+		super();
 
-	return (
-		<div style={{ width: 700 }} className="container mt-4 shadow-lg p-3 mb-5 bg-white rounded">
-			{todoItems}
-		</div>
-	);
-};
+		this.state = {
+			todos: todosData
+		};
+
+		this.handleChange = this.handleChange.bind(this);
+	}
+
+	handleChange(id) {
+		this.setState((prevState) => {
+			const todos = prevState.todos.map((item) => {
+				if (item.id === id) {
+					item.completed = !item.completed;
+				}
+				return item;
+			});
+			return {
+				todos
+			};
+		});
+		console.log(this.state);
+	}
+
+	render() {
+		const todoItems = this.state.todos.map((item) => (
+			<TodoItems handleChange={this.handleChange} key={item.id} item={item} />
+		));
+
+		return (
+			<div style={{ width: 700 }} className="container mt-4 shadow-lg p-3 mb-5 bg-white rounded">
+				{todoItems}
+			</div>
+		);
+	}
+}
 
 export default App;
